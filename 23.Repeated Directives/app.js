2e6ea29c777025c35d18f70810425cf01024cf65
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-11-16 14:34:29 +0530

    custom directive & Advanced concepts of custom directive

---
+++ 23.Repeated Directives/app.js
@@ -0,0 +1,81 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider) {
+
+    $routeProvider
+
+        .when('/', {
+            templateUrl: 'pages/main.html',
+            controller: 'mainController'
+        })
+
+        .when('/second', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
+
+        .when('/second/:num', {
+            templateUrl: 'pages/second.html',
+            controller: 'secondController'
+        })
+
+});
+
+myApp.controller('mainController', ['$scope', '$log', function ($scope, $log) {
+
+    $scope.peoples = [
+        {
+            name: 'John Doe',
+            address: '555 Main St.',
+            city: 'New York',
+            state: 'NY',
+            zip: '11111'
+        },
+        {
+            name: 'Jane Doe',
+            address: '333 Second St.',
+            city: 'Buffalo',
+            state: 'NY',
+            zip: '2222'
+        },
+        {
+            name: 'George Doe',
+            address: '111 Third St.',
+            city: 'Miami',
+            state: 'FL',
+            zip: '3333'
+        },
+        {
+            name: 'Emma Doe',
+            address: '222 Fourth St.',
+            city: 'Alaska',
+            state: 'AL',
+            zip: '44444'
+        }
+    ];
+
+    $scope.formattedAddress = function (person) {
+
+        return person.address + ', ' + person.city + ', ' + person.state + ' ' + person.zip;
+
+    };
+
+}]);
+
+myApp.controller('secondController', ['$scope', '$log', '$routeParams', function ($scope, $log, $routeParams) {
+
+
+
+}]);
+
+myApp.directive("searchResult", function () {
+    return {
+        restrict: 'AECM',
+        templateUrl: 'directives/searchresult.html',
+        replace: true,
+        scope: {
+            personObject: "=",
+            formattedAddressFunction: "&"
+        }
+    }
+});
