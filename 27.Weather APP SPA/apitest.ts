c5e2b8dfe2f787bc2cc0d15da6aded85adf5c7e9
Udayaditya Singh <udayaditya.singh@gmail.com>
2018-11-17 13:12:30 +0530

    Weather APP SPA

---
+++ 27.Weather APP SPA/apitest.ts
@@ -0,0 +1,5 @@
+'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=4b2967480a3acac2bf0a7f540d6fbb6c'
+
+
+// With API Key code
+// https://github.com/atufkas/angular-openweather-app
